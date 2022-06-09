8ec54f1a773bd87f77a788aab33c3366485eb18c
JessicaQuintino <61276103+JessicaQuintino@users.noreply.github.com>
2022-06-08 21:40:41 -0300

    Add files via upload

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => (
+    <header className="content">
+        <p>Sistema de supervisão esteira industrial</p>
+    </header>
+);
+
+export default Header;
