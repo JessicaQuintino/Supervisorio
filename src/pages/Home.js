73c3d0f0e88985f7cb3019ed832bce05290dc38d
JessicaQuintino <61276103+JessicaQuintino@users.noreply.github.com>
2022-06-08 21:39:39 -0300

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Dashboard from "../socketDashboard";
+
+const Home = () => (
+    <div className="title">
+        <Dashboard/>
+    </div>
+);
+
+export default Home;
