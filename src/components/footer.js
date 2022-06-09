22523f5cf7374e249e3cf73acd6ef25755c547ad
JessicaQuintino <61276103+JessicaQuintino@users.noreply.github.com>
2022-06-09 14:16:18 -0300

    Delete src directory

--- src/components/footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Footer = () => (
-    <footer className="content">
-        <h5>&copy; 2022</h5>
-    </footer>
-);
-
-export default Footer;
--- src/components/header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Header = () => (
-    <header className="content">
-        <p>Sistema de supervisão esteira industrial</p>
-    </header>
-);
-
-export default Header;
